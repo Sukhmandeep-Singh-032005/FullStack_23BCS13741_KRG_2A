a1b6c3c39da8e1b4bebcacf634928f6d337f1738
Sukhmandeep Singh <soemrndomguy@gmail.com>
2025-11-03 20:13:44 +0530

    Delete Experiment 4/Components directory

--- Experiment 4/Components/Loader.jsx
+++
@@ -1,7 +0,0 @@
-export default function Loader() {
-  return (
-    <div className="flex justify-center items-center h-64">
-      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-purple-500 border-opacity-50"></div>
-    </div>
-  );
-}
