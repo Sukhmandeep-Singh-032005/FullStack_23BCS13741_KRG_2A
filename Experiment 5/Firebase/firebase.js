cfc37be51333199dceccb312a6781a3667ddba2b
Sukhmandeep Singh <soemrndomguy@gmail.com>
2025-11-03 20:11:31 +0530

    Create firebase.js

---
+++ Experiment 5/Firebase/firebase.js
@@ -0,0 +1,22 @@
+import { createContext, useState, useEffect } from 'react';
+
+export const ThemeContext = createContext();
+
+export function ThemeProvider({ children }) {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    if (darkMode) {
+      root.classList.add('dark');
+    } else {
+      root.classList.remove('dark');
+    }
+  }, [darkMode]);
+
+  return (
+    <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
