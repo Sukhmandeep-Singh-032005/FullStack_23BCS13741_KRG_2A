f2d5fbf891e3e1a3bd9aa4ef1a0740cb99efa560
Sukhmandeep-Singh-032005 <soemrndomguy@gmail.com>
2025-11-15 16:19:26 +0530

    EST practical uploaded!!!

---
+++ Final EST/my-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import Amaterasu from "./itachi";
+import Increment from "./increment.jsx";
+import React, { useState } from "react";
+
+
+function App() {
+
+  return(
+    <div>
+      <Increment></Increment>
+    </div>
+  );
+
+}
+
+export default App
